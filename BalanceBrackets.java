9ca50d1ca33ef38ebc27490241987879efbc68c2
TanmayAjeetKumar <97678096+TanmayAjeetKumar@users.noreply.github.com>
2022-03-18 20:09:48 +0530

    Update BalanceBrackets.java

--- BalanceBrackets.java
+++ BalanceBrackets.java
@@ -1,31 +1,27 @@
-// Java program for checking
-// balanced brackets
+
 import java.util.*;
  
-public class BalancedBrackets {
+public class BalanceBrackets {
  
-    // function to check if brackets are balanced
+ 
     static boolean areBracketsBalanced(String expr)
     {
-        // Using ArrayDeque is faster than using Stack class
         Deque<Character> stack
             = new ArrayDeque<Character>();
  
-        // Traversing the Expression
+     
         for (int i = 0; i < expr.length(); i++)
         {
             char x = expr.charAt(i);
  
             if (x == '(' || x == '[' || x == '{')
             {
-                // Push the element in the stack
+               
                 stack.push(x);
                 continue;
             }
  
-            // If current character is not opening
-            // bracket, then it must be closing. So stack
-            // cannot be empty at this point.
+           
             if (stack.isEmpty())
                 return false;
             char check;
@@ -50,19 +46,19 @@ public class BalancedBrackets {
             }
         }
  
-        // Check Empty Stack
+    
         return (stack.isEmpty());
     }
  
-    // Driver code
+
     public static void main(String[] args)
     {
     	Scanner sc = new Scanner(System.in);
 		System.out.println("Enter the Parenthesis");
-		//int steps=sc.nextInt();
+		
         String expr = sc.next();
  
-        // Function call
+       
         if (areBracketsBalanced(expr))
             System.out.println("Balanced ");
         else
